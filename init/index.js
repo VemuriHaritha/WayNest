73bbb801bb96a5e81e7ed08b3c655f7cfeee16cb
Vemuri Haritha <161494669+VemuriHaritha@users.noreply.github.com>
2025-06-04 11:11:07 +0530

    Add files via upload

---
+++ init/index.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://localhost:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  try {
+    await Listing.deleteMany({});
+    const updatedData = initData.data.map((obj) => ({
+      ...obj,
+      owner: "677a3edc4acaecb30c87a507",
+    }));
+    await Listing.insertMany(updatedData);
+    console.log("Data was initialized successfully.");
+  } catch (err) {
+    console.error("Error initializing data:", err);
+  }
+};
+
+// const initDB = async () => {
+//   await Listing.deleteMany({});
+//   initData.data = initData.data.map((obj)=> ({
+//     ...obj, 
+//     owner:"6778f5bdeb3b766e3f69b182",
+//   })); 
+//   await Listing.insertMany(initData.data);
+//   console.log("data was initialized");
+// };
+
+initDB();
